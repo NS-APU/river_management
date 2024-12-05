e46f7a59f895152754a1e03001cd8b88dd546d42
PiNzero0 <longdou0@gmail.com>
2024-12-05 15:27:00 +0900

    [fix] access token, env variable

--- zabbix_server/line_notify.sh
+++ zabbix_server/line_notify.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Zabbixから件名とメッセージ内容を受け取るシェルスクリプト
 
-ACCESS_TOKEN="ck+079memyog8u4XwATz8b50nSxhlAWESQIUofY5+K46odstCrpVzlEq9KK34IWGZjgSirdOV5XZgFIYJwAn5qBn90xVq91UF7J010tf6fxRCI/mZsE765OahLcYUcnBBLpj7yLfN2qCqMF3xjRD5QdB04t89/1O/w1cDnyilFU="  # LINE公式アカウントのアクセストークン
+source .env
+
 SUBJECT=$1  # Zabbixから渡される件名
 MESSAGE=$2  # Zabbixから渡されるメッセージ内容
 
